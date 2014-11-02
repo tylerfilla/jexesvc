d767e5bff054c9894760fa1d49e96c2edec7437a
tylerfilla <tylerfilla@gmail.com>
2014-11-02 15:30:32 -0600

    Update version

--- versioning.h
+++ versioning.h
@@ -2,10 +2,10 @@
 #define _VERSIONING_H_
 
 	//Date Version Types
-	static const char VERSION_DATE[] = "14";
-	static const char VERSION_MONTH[] = "07";
+	static const char VERSION_DATE[] = "02";
+	static const char VERSION_MONTH[] = "11";
 	static const char VERSION_YEAR[] = "2014";
-	static const char VERSION_UBUNTU_VERSION_STYLE[] =  "14.07";
+	static const char VERSION_UBUNTU_VERSION_STYLE[] =  "14.11";
 	
 	//Software Status
 	static const char VERSION_STATUS[] =  "";
@@ -14,14 +14,14 @@
 	//Standard Version Type
 	static const long VERSION_MAJOR  = 0;
 	static const long VERSION_MINOR  = 0;
-	static const long VERSION_BUILD  = 425;
-	static const long VERSION_REVISION  = 2373;
+	static const long VERSION_BUILD  = 428;
+	static const long VERSION_REVISION  = 2388;
 	
 	//Miscellaneous Version Types
-	static const long VERSION_BUILDS_COUNT  = 748;
-	#define VERSION_RC_FILEVERSION 0,0,425,2373
-	#define VERSION_RC_FILEVERSION_STRING "0, 0, 425, 2373\0"
-	static const char VERSION_FULLVERSION_STRING [] = "0.0.425.2373";
+	static const long VERSION_BUILDS_COUNT  = 759;
+	#define VERSION_RC_FILEVERSION 0,0,428,2388
+	#define VERSION_RC_FILEVERSION_STRING "0, 0, 428, 2388\0"
+	static const char VERSION_FULLVERSION_STRING [] = "0.0.428.2388";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long VERSION_BUILD_HISTORY  = 0;
